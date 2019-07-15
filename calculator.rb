a91954dfba59e4130fb1501f6adb145af4cd4d7b
Marc Blanchard <marcblanchard0930@gmail.com>
2019-07-15 17:20:32 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,6 +4,6 @@ second_number = 6
 
 sum = first_number + second_number
 difference = first_number - second_number
-product = first_number - second_number
+product = first_number * second_number
 quotient = first_number / second_number
 
